37990c8dcad2d04cb511e771d9368d6beb147425
Nguyen Ngoc Tung <ngoctungn8@gmail.com>
2023-04-02 20:41:54 +0700

    update: use viewBinding to set layout

--- Application/app/src/main/java/com/khtn/plant_app/LoginActivity.kt
+++ Application/app/src/main/java/com/khtn/plant_app/LoginActivity.kt
@@ -2,10 +2,16 @@ package com.khtn.plant_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import com.khtn.plant_app.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
