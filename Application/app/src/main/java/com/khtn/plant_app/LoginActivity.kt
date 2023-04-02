8a549a89241777c2800e4dc71f2242cbbb6f475d
Nguyen Ngoc Tung <ngoctungn8@gmail.com>
2023-04-02 21:04:08 +0700

    update: test login code using firebase auth

--- Application/app/src/main/java/com/khtn/plant_app/LoginActivity.kt
+++ Application/app/src/main/java/com/khtn/plant_app/LoginActivity.kt
@@ -1,8 +1,12 @@
 package com.khtn.plant_app
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.khtn.plant_app.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
@@ -13,5 +17,33 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        // Initialize Firebase Auth
+        auth = Firebase.auth
+
+        binding.buttonLogin.setOnClickListener{
+            val email = binding.textfieldEmail.editText?.text.toString()
+            val password = binding.textfieldPassword.editText?.text.toString()
+
+            if(email.isNotEmpty() && password.isNotEmpty()){
+                auth.signInWithEmailAndPassword(email, password).addOnCompleteListener{
+                    if(it.isSuccessful){
+                        val intent = Intent(this, MainActivity::class.java)
+                        startActivity(intent)
+                    }
+                    else
+                    {
+                        Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+            else
+            {
+                Toast.makeText(this, "Email and password cannot be empty", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+
+
     }
 }
