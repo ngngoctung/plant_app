e6be164bd1b36537a431659234bc35009253095f
Viet Tuan <tuanttran135@gmail.com>
2023-05-13 19:19:26 +0700

    update: add fragment transition in home.kt

--- Application/app/src/main/java/com/khtn/plant_app/Home.kt
+++ Application/app/src/main/java/com/khtn/plant_app/Home.kt
@@ -1,18 +1,26 @@
 package com.khtn.plant_app
 
+import android.app.Activity.RESULT_OK
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
 import com.khtn.plant_app.databinding.FragmentHomeBinding
+
+
 
 class Home : Fragment() {
     private lateinit var binding:FragmentHomeBinding
     private lateinit var mContext: Context
     private lateinit var myPref: SessionManager
+    private val REQUEST_IMAGE_CAPTURE = 1
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -26,8 +34,40 @@ class Home : Fragment() {
             startActivity(intent)
             activity?.onBackPressed()
         }
+
+        binding.buttonSpecies.setOnClickListener{
+            val fragment = Species()
+            activity?.supportFragmentManager?.beginTransaction()?.replace(R.id.frame_layout, fragment)?.commit()
+        }
+
+        binding.buttonArticles.setOnClickListener{
+            val fragment = Species()
+            activity?.supportFragmentManager?.beginTransaction()?.replace(R.id.frame_layout, fragment)?.commit()
+        }
+
+        binding.buttonCamera.setOnClickListener{
+            dispatchTakePictureIntent()
+        }
+
+
         return binding.root
     }
+
+    private fun dispatchTakePictureIntent() {
+        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            val imageBitmap = data?.extras?.get("data") as Bitmap
+            // Làm gì đó với imageBitmap, ví dụ: hiển thị lên ImageView
+        }
+    }
+
+
 
     private fun initMypPref() {
         myPref = SessionManager(mContext)
