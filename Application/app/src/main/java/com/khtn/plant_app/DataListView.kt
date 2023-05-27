e58ac187de798c9206804a1dc7d14f6196dba44d
Viet Tuan <tuanttran135@gmail.com>
2023-05-27 11:22:32 +0700

    update: add adapter

---
+++ Application/app/src/main/java/com/khtn/plant_app/DataListView.kt
@@ -0,0 +1,9 @@
+package com.khtn.plant_app
+
+data class DataListView (
+    val lisviewImage: Int,
+    var name: String,
+    var kingdom: String,
+    var family: String,
+    var description: String
+)
