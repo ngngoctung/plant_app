7ed2ee70fe3841b90a80e3273aede32381ae48fc
bichtuyennn <vantuyen0425@gmail.com>
2023-05-29 02:01:35 +0700

    Species layout - update display list (add alphabet)

--- Application/app/src/main/java/com/khtn/plant_app/Species.kt
+++ Application/app/src/main/java/com/khtn/plant_app/Species.kt
@@ -3,6 +3,7 @@ package com.khtn.plant_app
 import android.annotation.SuppressLint
 import android.content.ContentValues.TAG
 import android.content.Context
+import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -14,6 +15,7 @@ import androidx.fragment.app.Fragment
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import com.khtn.plant_app.databinding.FragmentSpeciesBinding
+import kotlinx.android.synthetic.main.item_textview.view.name_textview
 
 
 class Species : Fragment() {
@@ -33,9 +35,9 @@ class Species : Fragment() {
         myPref = SessionManager(mContext)
 
         var name:MutableList<String> = mutableListOf()
-
-//        "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N",
-//        "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"
+        var alphabet: List<String> = listOf("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N",
+            "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z")
+        var speciesNameList: MutableList<String> = mutableListOf()
 
         var i = 0
         db.collection("Plants")
@@ -57,9 +59,37 @@ class Species : Fragment() {
 
                 }
                 var distinctList = name.distinct().sortedWith(String.CASE_INSENSITIVE_ORDER)
-                Log.d(TAG, distinctList[0] + distinctList[1] + distinctList[2] + distinctList[3])
-                val arrayAdapter:ArrayAdapter<String> = ArrayAdapter(mContext, android.R.layout.simple_list_item_1, distinctList)
-                Log.d(TAG, "===================")
+
+//                Log.d(TAG, "------------------------")
+//                for(j in 0..(distinctList.size-1)){
+//                    Log.d(TAG, distinctList[j])
+//                }
+
+                var flag: Int = 0   //flag keep position list
+                var count = distinctList.size   //reduce the number of loop when end of distinctList's list
+                for(l in 0..alphabet.size){
+                    if(count == 0)
+                        break
+                    //binding.listView.name_textview.setTextColor(Color.parseColor("#2DDA93"))
+                    speciesNameList.add(flag, alphabet[l])
+                    flag++
+                    for(k in 0 until distinctList.size){
+                        Log.d(TAG, "---- k  =  " + k)
+                        if(distinctList[k].startsWith(alphabet[l])) {
+                            speciesNameList.add(flag, distinctList[k])
+                            flag++
+                            count--
+                        } else {
+                            continue
+                        }
+
+                    }
+                }
+//                Log.d(TAG, "------------------------")
+//                for(j in 0..(distinctList.size-1)){
+//                    Log.d(TAG, speciesNameList[j])
+//                }
+                val arrayAdapter:ArrayAdapter<String> = ArrayAdapter(mContext, android.R.layout.simple_list_item_1, speciesNameList)
                 binding.listView.adapter = arrayAdapter
                 binding.listView.setOnItemClickListener { _, _, i, _ ->
                     Toast.makeText(mContext, "Item selected " + distinctList[i], Toast.LENGTH_SHORT).show()
